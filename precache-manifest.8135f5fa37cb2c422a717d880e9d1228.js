fcbefb00a9bb44f4dc96c3526e8e95b569e850f6
anbesajer pawlos <ambesa4@gmail.co>
2020-05-24 15:35:30 -0700

    Updates

---
+++ precache-manifest.8135f5fa37cb2c422a717d880e9d1228.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1c744f0181b5af67ca18153d1ab6948",
+    "url": "/covidtracker/index.html"
+  },
+  {
+    "revision": "b79ddfc4d4791601ad0b",
+    "url": "/covidtracker/static/css/main.3cb6394e.chunk.css"
+  },
+  {
+    "revision": "4663e568c79c71da8156",
+    "url": "/covidtracker/static/js/2.90843bb1.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covidtracker/static/js/2.90843bb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b79ddfc4d4791601ad0b",
+    "url": "/covidtracker/static/js/main.4e8ee177.chunk.js"
+  },
+  {
+    "revision": "f7a6649f8aa699973460",
+    "url": "/covidtracker/static/js/runtime-main.90f90e6f.js"
+  }
+]);
